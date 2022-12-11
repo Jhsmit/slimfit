20db21fec2286ff132f427d2437e57366ffd473a
Jochem Smit <jhsmit@gmail.com>
2022-12-11 13:43:15 +0100

    versioneer path

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 import setuptools
 import versioneer
 
+# ensure the current directory is on sys.path so versioneer can be imported
+# when pip uses PEP 517/518 build rules.
+# https://github.com/python-versioneer/python-versioneer/issues/193
+sys.path.append(os.path.dirname(__file__))
+
 if __name__ == "__main__":
     setuptools.setup(
         version=versioneer.get_version(),

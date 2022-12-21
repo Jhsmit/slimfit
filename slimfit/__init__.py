7caab2df72cb6918cbb0e16488252a7ae7ce8989
Jochem Smit <jhsmit@gmail.com>
2022-12-21 11:32:50 +0100

    comments

--- slimfit/__init__.py
+++ slimfit/__init__.py
@@ -4,11 +4,13 @@ from slimfit.fit import Fit
 from slimfit.symbols import Symbol, FitSymbol
 from slimfit.parameter import Parameters, Parameter
 
+# placeholder version number
 __version__ = "0.0.0"
 
+# when we are on editable install from source, the _version file is present
+# and we can get a version from there
 try:
     from . import _version
-
     __version__ = _version.get_versions()["version"]
 except ImportError:
     pass

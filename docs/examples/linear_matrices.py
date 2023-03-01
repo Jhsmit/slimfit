d17055c9f8134e8f7157fcd00bd8c8a603b55dc6
Jochem Smit <jhsmit@gmail.com>
2023-03-01 16:43:58 +0100

    mardown comments in example

--- docs/examples/linear_matrices.py
+++ docs/examples/linear_matrices.py
@@ -1,3 +1,10 @@
+# %% [markdown]
+#
+# This example assumes a measured spectrum which is a linear combination of known basis vectors
+# (which are here modelled as Gaussians). The goal is to find the coefficients of the linear combinations.
+#
+
+# %%
 import numpy as np
 
 from slimfit import Model
@@ -10,13 +17,12 @@ from slimfit.symbols import symbol_matrix, Symbol
 
 import proplot as pplt
 
-"""
-In this example we have a measured spectrum which consist of a linear combination of known basis vectors and 
-want to find the coefficients of these linear combinations. 
-"""
+# %% [markdown]
+#
+# Generating the basis vectors from Gaussians, create spectrum from given ground-truth coefficients.
+#
 
 # %%
-
 # Generate the basis vectors, modelled as gaussian peaks
 mu_vals = [1.1, 3.5, 7.2]
 sigma_vals = [0.25, 0.1, 0.72]
@@ -24,69 +30,58 @@ wavenumber = np.linspace(0, 11, num=100)  # wavenumber x-axis
 basis = np.stack(
     [gaussian_numpy(wavenumber, mu_i, sig_i) for mu_i, sig_i in zip(mu_vals, sigma_vals)]
 ).T
-
+basis.shape
 # %%
-
 # Ground-truth coefficients we want to find
 x_vals = np.array([0.3, 0.5, 0.2]).reshape(3, 1)  # unknowns
 
 # Simulated measured spectrum given ground-truth coefficients and basis vectors
 spectrum = basis @ np.array(x_vals).reshape(3, 1)
-
 spectrum += np.random.normal(0, 0.1, size=spectrum.shape)  # add noise
 
 spectrum.shape
+
+# %% [markdown]
+#
+# The model describing the spectrum is of form $Ax = b$; where $A$ is the matrix of stacked basis vectors, $x$ is the
+# coefficient vector we want to find and $b$ is the measured spectrum.
+#
+# We can define the model in two ways:
+#
+# **Option 1**: Create sympy Matrix with coefficients and multiply it with the array of coefficients.
+
 # %%
-
-"""
-The model describing the spectrum is of from Ax = b; where A is our matrix of stacked basis vectors, x is the 
-coefficient vector we want to find and b is the measured spectrum.
-
-We can define the model in two ways: 
-"""
-
-# %%
-"""
-Option 1: Create sympy Matrix with coefficients and multiply it with the array of coefficients
-"""
 # Create a sympy matrix with parameters are elements with shape (3, 1)
 x = symbol_matrix(name="X", shape=(3, 1))
-
 parameters = Parameters.from_symbols(x.free_symbols)
-x, parameters
+x, type(x)
 
 # %%
-m = basis @ x  # Matirx multiply basis matrix with parameter vector
+# Matrix multiply basis matrix with parameter vector
+m = basis @ x
+# define the model, measured spectrum corresponds to Symbol('b')mod
 model = Model(
     {Symbol("b"): basis @ x}
-)  # define the model, measured spectrum corresponds to Symbol('b')
-num_expr = model.expr[Symbol("b")]
-num_expr.shape
-
+)
+type(model[Symbol("b")])
 
 # %%
 fit = Fit(model, parameters, data={"b": spectrum})
 result = fit.execute()  # execution time 117 ms
 result.parameters
 
-# %%
-
-"""
-This works but is performance-wise not desirable as the MatrixNumExpr in the model is shape (100, 1) and calling it
-requires evaluating one lambdified function per matrix element.
-"""
-
-# %%
-
-"""
-Option 2: Create a (3x1) Matrix and evaluate the matrix multiplication lazily with `MatMul`
-"""
-
+# %% [markdown]
+#
+# This works but is performance-wise not desirable as the matrix in the model is shape (100, 1). Calling the model
+# currently is implemented by lambdifying the matrix element-wise, this requires 100 lambda functions to be evaluated,
+# and their result to be placed in an array through a for loop.
+#
+# **Option 2**: Evaluate the matrix multiplication lazily with `MatMul`
+#
 # %%
 m = MatMul(basis, x)
 model = Model({Symbol("b"): m})
-m_callable = model.expr[Symbol("b")]
-m_callable  # = MatMul object
+type(model[Symbol("b")])
 #
 # %%
 fit = Fit(model, parameters, data={"b": spectrum})
@@ -96,9 +91,15 @@ result = fit.execute()  # execution time: 15.2 ms
 for i, j in np.ndindex(x_vals.shape):
     print(x_vals[i, j], result.parameters[f"X_{i}_{j}"])
 
-
+# %% [markdown]
+#
+# The second option is 10 times faster as only a 3x1 matrix is evaluated through element-wise lambdification. The
+# matrix multiplication step is done in numpy, and is fast.
+#
+# Plotting the results:
+# 
 # %%
-# plot the results
+
 fig, ax = pplt.subplots()
 ax.plot(wavenumber, spectrum, color="k")
 ax.plot(wavenumber, model(**result.parameters)["b"], color="r", lw=1, alpha=0.75)

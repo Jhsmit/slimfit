e3b977e493153f51b9288887fa03f5244f5271e7
Jochem Smit <jhsmit@gmail.com>
2023-08-07 10:51:17 +0200

    Switch to hatch (#16)
    * switch to hatch
    
    * update pytest, pin requirements with pip-compile
    
    * generate requirements file per OS
    
    * drop py38
    
    * add missing version file
    
    * remove playwright dependency
    
    * prefer binary pip install
    
    * docs workflow
    
    * always install dependencies
    
    * workflow for pinning requirements
    
    * on push
    
    * versions as strings
    
    * bump version
    
    * add pinned requirements versions
    
    * add dependabot config
    
    * add authors
    
    * fix dependabot file
    
    * split test and lint
    
    * update docs workflow

--- slimfit/__init__.py
+++ slimfit/__init__.py
@@ -6,14 +6,4 @@ from slimfit.symbols import Symbol, FitSymbol, symbol_matrix, clear_symbols
 from slimfit.parameter import Parameters, Parameter
 from slimfit.operations import Add, Mul, Sum
 
-# placeholder version number
-__version__ = "0.0.0"
-
-# when we are on editable install from source, the _version file is present
-# and we can get a version from there
-try:
-    from . import _version
-
-    __version__ = _version.get_versions()["version"]
-except ImportError:
-    pass
+from slimfit.__version__ import __version__  # noqa: F401
